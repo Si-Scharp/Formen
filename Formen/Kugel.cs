57de9b8265cd847f3cd04ef4c4c299e66bf34644
davidbeh <david.behres@gmail.com>
2021-05-12 15:59:23 +0200

    Projektdateien hinzufügen.

---
+++ Formen/Kugel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formen
+{
+
+    public class Kugel
+    {
+        private static double NthRoot(double A, int N)
+        {
+            return Math.Pow(A, 1.0d / N);
+        }
+
+        double _radius;
+        double _durchmesser;
+        double _umfang;
+        double _volumen;
+        double _oberfläche;
+
+        public static Kugel vonRadius(double radius) => new Kugel() { _radius = radius };
+        public static Kugel vonDurchmesser(double durchmesser) => new Kugel() { _radius = durchmesser / 2d };
+        public static Kugel vonUmfang(double umfang) => new Kugel() { _radius = umfang / ( 2d * Math.PI) };
+        public static Kugel vonVolumen(double volumen) => new Kugel() { _radius = NthRoot((3 * volumen) / (4d * Math.PI), 3) };
+        public static Kugel vonOberfläche(double oberfläche) => new Kugel() { _radius = Math.Sqrt(oberfläche / (4d * Math.PI)) };
+
+        public double Radius => _radius;
+        public double Durchmesser => _radius * 2d;
+        public double Umfang => 2d * _radius * Math.PI;
+        public double Volumen => (4d / 3d) * Math.PI * Math.Pow(_radius, 3);
+        public double Oberfläche => 4d * Math.PI * Math.Pow(_radius, 2);
+    }
+}
---
+++ FormenKonsole/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using Formen;
+namespace FormenKonsole
+{
+    enum KreisParameter
+    {
+        radius,
+        durchmesser,
+        umfang,
+        volumen,
+        oberfläche,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Wähle den Parameter aus dem die anderen Werte berechnen werden sollen:");
+                Console.WriteLine("Gebe ein: r für Radius");
+                Console.WriteLine("Gebe ein: d für Durchmesser");
+                Console.WriteLine("Gebe ein: u für Umfang");
+                Console.WriteLine("Gebe ein: V für Volumen");
+                Console.WriteLine("Gebe ein: O für Oberfläche");
+                string aktion = Console.ReadLine();
+
+                KreisParameter parameter;
+
+                if (aktion == "r") parameter = KreisParameter.radius;
+                else if (aktion == "d") parameter = KreisParameter.durchmesser;
+                else if (aktion == "u") parameter = KreisParameter.umfang;
+                else if (aktion == "V") parameter = KreisParameter.volumen;
+                else if (aktion == "O") parameter = KreisParameter.oberfläche;
+                else
+                {
+                    Console.Error.WriteLine("Etwas ist schiefgelaufen. Drücke eine beliebige Taste");
+                    Console.ReadKey();
+                    continue;
+                }
+                double wert;
+                Console.WriteLine("\nGebe nun den Wert für diese Parameter ohne Einheit ein");
+                try
+                {
+                    wert = double.Parse(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine("Etwas ist schiefgelaufen. Drücke eine beliebige Taste");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                Kugel k = null;
+
+                try
+                {
+                    switch (parameter)
+                    {
+                        case KreisParameter.radius:
+                            k = Kugel.vonRadius(wert);
+                            break;
+                        case KreisParameter.durchmesser:
+                            k = Kugel.vonDurchmesser(wert);
+                            break;
+                        case KreisParameter.umfang:
+                            k = Kugel.vonUmfang(wert);
+                            break;
+                        case KreisParameter.volumen:
+                            k = Kugel.vonVolumen(wert);
+                            break;
+                        case KreisParameter.oberfläche:
+                            k = Kugel.vonOberfläche(wert);
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine("Etwas ist bei der Erstellung der Kugel schiefgelaufen. Drücke eine beliebige Taste");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                Console.Clear();
+                Console.WriteLine($"Das sind die Parametere für eine Kugel mit einem {parameter.ToString().FirstCharToUpper()} von {wert}:");
+                Console.WriteLine();
+                try
+                {
+                    Console.WriteLine("Radius      r: " + k.Radius);
+                    Console.WriteLine("Durchmesser d: " + k.Durchmesser);
+                    Console.WriteLine("Umfang      u: " + k.Umfang);
+                    Console.WriteLine("Volumen     V: " + k.Volumen);
+                    Console.WriteLine("Oberfläche  O: " + k.Oberfläche);
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine("Etwas ist bei der Berechnung der anderen Parameter schiefgelaufen. Drücke eine beliebige Taste");
+                    Console.ReadKey();
+                    continue;
+                }
+                Console.WriteLine("\nDrücke eine beliebige Taste um eine neue Kugel zu berechnen");
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input) =>
+            input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+                _ => input.First().ToString().ToUpper() + input.Substring(1)
+            };
+    }
+}
